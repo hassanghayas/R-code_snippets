778a2185645c54e036413e3ce8d90e3d763b7d11
hassan ghayas <hassan_ms@outlook.com>
2026-01-31 19:55:32 +0500

    map script

--- map.R
+++ map.R
@@ -3,84 +3,65 @@ library(sf)
 library(tmap)
 library(ggspatial)
 
+# download shape file
+# https://data.humdata.org/dataset/cod-ab-pak
 
 # Load shapefile
-cities <- st_read("PAK_adm2.shp")
-district <- st_read("PAK_adm3.shp")
-x1 <- st_read("gis_osm_places_free_1.shp")
-x <- st_read("gis_osm_places_a_free_1.shp")
-zz <- st_read("pak_admbnda_adm3_wfp_20220909.shp")
-z <- st_read("pak_admbndp_admALL_wfp_itos_20220909.shp")
+Pak.shp.lv1 <- st_read("pak_admbnda_adm0_wfp_20220909.shp") # country
+Pak.shp.lv2 <- st_read("pak_admbnda_adm1_wfp_20220909.shp") # states
+Pak.shp.lv3 <- st_read("pak_admbnda_adm2_wfp_20220909.shp") # district
+Pak.shp.lv4 <- st_read("pak_admbnda_adm3_wfp_20220909.shp") # town
 
-
-# View the data structure
-print(cities)
-names(cities)
-
-# Filter a specific city (e.g., Karachi)
-karachi <- cities[cities$NAME_2 == "Karachi", ]
-sindh <- district[district$NAME_1 == "Sind",]
-karachi2 <- district[district$NAME_2 == "Karachi",]
-
-zz_sindh <- zz[zz$ADM1_EN == "Sindh",]
-Khi <- zz_sindh[zz_sindh$ADM2_EN %in% c("Jamshoro","Thatta","West Karachi","East Karachi","South Karachi","Central Karachi","Korangi Karachi","Malir Karachi"),]
-khi2 <- Khi[!(Khi$ADM3_EN %in% c("Thatta","Keti Bunder","Ghorabari","Kotri","Manjhand","Sehwan Sharif")),]
-
-P_khi <-ggplot() +
-  geom_sf(data = khi2, fill = "gray85", color = "black")+
+# Pak map, state level
+Pak_map.L2 <- ggplot() +
+  geom_sf(data = Pak.shp.lv2, fill = "gray85", color = "black")+
+  geom_sf_text(data = Pak.shp.lv2, aes(label = ADM1_EN), size = 2, color = "black")+
   annotation_scale(location = "bl", width_hint = 0.25) +
-  theme(panel.background = element_rect(fill = "white"),axis.ticks=element_blank(),axis.text=element_blank())
-P_khi
-P_khi+geom_point(data = all_data, aes(x = lon, y = lat, size = sample_size), color = "red") 
+  theme(panel.grid = element_line(color = "black",linewidth = 0.2),
+        panel.background = element_rect(fill = "white", color="black"),
+        axis.title = element_blank())
 
+# Pak_map.L2 <- ggplot() +
+#   geom_sf(data = Pak.shp.lv2, fill = "gray85", color = "black")+
+#   geom_sf_text(data = Pak.shp.lv2, aes(label = ADM1_EN), size = 2, color = "black")+
+#   annotation_scale(location = "bl", width_hint = 0.25) +
+#   theme(panel.background = element_rect(fill = "white"),axis.ticks=element_blank(),axis.text=element_blank())
 
+Pak_map.L2
 
-#plot the map
-P_city <-ggplot() +
-  geom_sf(data = cities, fill = "gray85", color = "black")+
-  geom_sf(data = karachi, fill = "blue", color = "black") +
+# filter sindh and Karachi data at district level
+sindh.L3 <- Pak.shp.lv3[Pak.shp.lv3$ADM1_EN == "Sindh",]
+khi.L3 <- Pak.shp.lv3[Pak.shp.lv3$ADM2_EN %in% c("West Karachi",
+                                                 "East Karachi",
+                                                 "South Karachi",
+                                                 "Central Karachi",
+                                                 "Korangi Karachi",
+                                                 "Malir Karachi"),]
+# sindh map with Karachi highlighted
+sindh_map.L3 <- ggplot() +
+  geom_sf(data = sindh.L3, fill = "gray85", color = "black")+
+  geom_sf(data = khi.L3, fill = "lightgreen", color = "black")+
+  annotate("text",x=67.2,y=25, label="karachi", size=2.5)+
   annotation_scale(location = "bl", width_hint = 0.25) +
-  theme(panel.background = element_rect(fill = "white"),axis.ticks=element_blank(),axis.text=element_blank())
-P_city
+  theme(panel.grid = element_blank(),panel.background = element_rect(fill = "white", color="black"))
+sindh_map.L3
 
-P_district <-ggplot() +
-  geom_sf(data = sindh, fill = "gray85", color = "black")+
-  geom_sf(data = karachi2, fill = "lightgreen", color = "black") +
+# filter karachi data at town level
+khi.L4 <- Pak.shp.lv4[Pak.shp.lv4$ADM2_EN %in% c("West Karachi",
+                                                 "East Karachi",
+                                                 "South Karachi",
+                                                 "Central Karachi",
+                                                 "Korangi Karachi",
+                                                 "Malir Karachi"),]
+
+# Karachi map at town level
+khi_map.L4 <- ggplot() +
+  geom_sf(data = khi.L4, fill = "grey85", color = "black")+
+  annotate("text",x=67.2,y=25, label="karachi", size=3.5)+
   annotation_scale(location = "bl", width_hint = 0.25) +
-  theme(panel.background = element_rect(fill = "white"),axis.ticks=element_blank(),axis.text=element_blank())
-P_district
+  theme(panel.grid = element_blank(),panel.background = element_rect(fill = "white", color="black"))
+khi_map.L4
 
-P_sindh <-ggplot() +
-  geom_sf(data = zz_sindh, fill = "gray85", color = "black")+
-  geom_sf_text(data = zz_sindh, aes(label = ADM3_EN), size = 3, color = "blue") +  # Add city names
-  annotation_scale(location = "bl", width_hint = 0.25) +
-  theme(panel.background = element_rect(fill = "white"),axis.ticks=element_blank(),axis.text=element_blank())
-P_sindh
+Pak_map.L2 +sindh_map.L3 +khi_map.L4
 
-
-
-
-# Load road shapefile
-roads <- st_read("PAK_roads.shp")
-
-# View the first few rows of the shapefile
-head(roads)
-
-#Query Roads from OpenStreetMap
-library(osmdata)
-
-# Query road data for Karachi
-karachi_roads <- opq("Karachi") %>%
-  add_osm_feature(key = "highway") %>%
-  osmdata_sf()
-
-# Extract road polygons
-roads <- karachi_roads$osm_lines
-roads1 <- roads[!is.na(roads$name), ]
-
-P_city_road <-ggplot() +
-  geom_sf(data = karachi, fill = "blue", color = "black") +
-  annotation_scale(location = "bl", width_hint = 0.25) +
-  geom_sf(data = roads1, color = "black", size = 0.3) +
-  theme(panel.background = element_rect(fill = "white"),axis.ticks=element_blank(),axis.text=element_blank())
-P_city_road
+ggsave("map.svg",dpi = 1200, width = 9, height = 4.7)
